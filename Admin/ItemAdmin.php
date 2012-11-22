11e1c52ce8c2d8898c6e297a0ceb7faa89206ed0
Tito Miguel Costa <github@titomiguelcosta.com>
2012-11-22 00:03:09 +0000

    translations and form validation

--- Admin/ItemAdmin.php
+++ Admin/ItemAdmin.php
@@ -1,62 +1,79 @@
 <?php
+
 namespace Zorbus\MenuBundle\Admin;
 
 use Sonata\AdminBundle\Admin\Admin;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
-use Sonata\AdminBundle\Validator\ErrorElement;
 use Sonata\AdminBundle\Form\FormMapper;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\MaxLength;
+use Symfony\Component\Validator\Constraints\Url;
+use Symfony\Component\Validator\Constraints\Type;
+use Symfony\Component\Validator\Constraints\Image;
 
 class ItemAdmin extends Admin
 {
+
     protected function configureFormFields(FormMapper $formMapper)
     {
         $formMapper
-            ->add('name')
-            ->add('url')
-            ->add('menu', null, array('required' => true, 'attr' => array('class' => 'span5 select2')))
-            ->add('parent', null, array('required' => false, 'attr' => array('class' => 'span5 select2')))
-            ->add('description', 'textarea', array('required' => false, 'attr' => array('class' => 'ckeditor')))
-            ->add('imageTemp', 'file', array('required' => false, 'label' => 'Image'))
-            ->add('enabled', null, array('required' => false))
+                ->add('name', null, array('constraints' => array(
+                        new NotBlank(),
+                        new MaxLength(array('limit' => 255))
+                    )
+                ))
+                ->add('url', null, array('constraints' => array(
+                        new NotBlank(),
+                        new Url()
+                    )
+                ))
+                ->add('menu', null, array(
+                    'required' => true,
+                    'attr' => array('class' => 'span5 select2'),
+                    'constraints' => array(
+                        new NotBlank(),
+                        new Type(array('type' => 'Zorbus\MenuBundle\Entity\Menu'))
+                    )
+                ))
+                ->add('parent', null, array(
+                    'required' => false,
+                    'attr' => array('class' => 'span5 select2'),
+                    'constraints' => array(
+                        new Type(array('type' => 'Zorbus\MenuBundle\Entity\Item'))
+                    )
+                ))
+                ->add('description', 'textarea', array('required' => false, 'attr' => array('class' => 'ckeditor')))
+                ->add('imageTemp', 'file', array(
+                    'required' => false,
+                    'label' => 'Image',
+                    'constraints' => array(
+                        new Image()
+                    )
+                ))
+                ->add('enabled', null, array('required' => false))
         ;
     }
 
     protected function configureDatagridFilters(DatagridMapper $datagridMapper)
     {
         $datagridMapper
-            ->add('name')
-            ->add('menu')
-            ->add('parent')
+                ->add('name')
+                ->add('menu')
+                ->add('parent')
         ;
     }
 
     protected function configureListFields(ListMapper $listMapper)
     {
         $listMapper
-            ->addIdentifier('name')
-            ->addIdentifier('menu')
-            ->addIdentifier('parent')
-            ->add('enabled')
+                ->addIdentifier('name')
+                ->addIdentifier('menu')
+                ->addIdentifier('parent')
+                ->add('enabled')
         ;
     }
 
-    public function validate(ErrorElement $errorElement, $object)
-    {
-        $errorElement
-            ->with('name')
-                ->assertNotBlank()
-                ->assertMaxLength(array('limit' => 255))
-            ->end()
-            ->with('url')
-                ->assertNotBlank()
-                ->assertMaxLength(array('limit' => 255))
-            ->end()
-            ->with('menu')
-                ->assertNotBlank()
-            ->end()
-        ;
-    }
     public function prePersist($object)
     {
         $object->setUpdatedAt(new \DateTime());
@@ -66,4 +83,5 @@ class ItemAdmin extends Admin
     {
         $object->setUpdatedAt(new \DateTime());
     }
+
 }
--- Admin/MenuAdmin.php
+++ Admin/MenuAdmin.php
@@ -1,46 +1,46 @@
 <?php
+
 namespace Zorbus\MenuBundle\Admin;
 
 use Sonata\AdminBundle\Admin\Admin;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
-use Sonata\AdminBundle\Validator\ErrorElement;
 use Sonata\AdminBundle\Form\FormMapper;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\MaxLength;
 
 class MenuAdmin extends Admin
 {
+
     protected function configureFormFields(FormMapper $formMapper)
     {
         $formMapper
-            ->add('title')
-            ->add('description', 'textarea', array('required' => false, 'attr' => array('class' => 'ckeditor')))
-            ->add('lang')
-            ->add('enabled', null, array('required' => false))
+                ->add('title', null, array('constraints' => array(
+                        new NotBlank(),
+                        new MaxLength(array('limit' => 255))
+                    )
+                ))
+                ->add('description', 'textarea', array('required' => false, 'attr' => array('class' => 'ckeditor')))
+                ->add('lang', 'language', array(
+                    'preferred_choices' => array('pt_PT', 'en')
+                ))
+                ->add('enabled', null, array('required' => false))
         ;
     }
 
     protected function configureDatagridFilters(DatagridMapper $datagridMapper)
     {
         $datagridMapper
-            ->add('title')
+                ->add('title')
         ;
     }
 
     protected function configureListFields(ListMapper $listMapper)
     {
         $listMapper
-            ->addIdentifier('title')
-            ->add('enabled')
+                ->addIdentifier('title')
+                ->add('enabled')
         ;
     }
 
-    public function validate(ErrorElement $errorElement, $object)
-    {
-        $errorElement
-            ->with('title')
-                ->assertNotBlank()
-                ->assertMaxLength(array('limit' => 255))
-            ->end()
-        ;
-    }
 }
