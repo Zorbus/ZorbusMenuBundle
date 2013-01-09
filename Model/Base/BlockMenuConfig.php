95aa1cdaa7f919362697d85eaac958e85d139b4d
Tito Miguel Costa <titomiguelcosta@gmail.com>
2013-01-09 11:41:31 +0000

    added base class to menu config

---
+++ Model/Base/BlockMenuConfig.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Zorbus\ZorbusBundle\Model\Menu\Base;
+
+use Zorbus\BlockBundle\Model\BlockConfig;
+
+abstract class BlockMenuConfig extends BlockConfig
+{
+    protected $httpKernel;
+    protected $em;
+
+    public function getMenusAsArray()
+    {
+        $objects = $this->em->getRepository('ZorbusMenuBundle:Menu')->findAll();
+        $menus = array();
+        foreach ($objects as $menu) {
+            $menus[$menu->getId()] = $menu->getTitle();
+        }
+
+        return $menus;
+    }
+
+}
--- Model/BlockMenuConfig.php
+++ Model/BlockMenuConfig.php
@@ -5,37 +5,37 @@ namespace Zorbus\MenuBundle\Model;
 use Zorbus\BlockBundle\Entity\Block as BlockEntity;
 use Sonata\AdminBundle\Admin\AdminInterface;
 use Symfony\Component\Form\FormFactory;
-use Zorbus\BlockBundle\Model\BlockConfig;
 
-class BlockMenuConfig extends BlockConfig
+class BlockMenuConfig extends Base\BlockMenuConfig
 {
 
-    public function __construct(AdminInterface $admin, FormFactory $formFactory, $httpKernel)
+    public function __construct(AdminInterface $admin, FormFactory $formFactory, $httpKernel, $em)
     {
         parent::__construct('zorbus_menu.block.menu', 'Menu Block', $admin, $formFactory);
         $this->enabled = true;
         $this->themes = array('ZorbusMenuBundle:Block:default' => 'Default');
         $this->httpKernel = $httpKernel;
+        $this->em = $em;
     }
 
     public function getFormMapper()
     {
         return $this->formMapper
-                        ->add('menu', 'entity', array(
-                            'class' => 'Zorbus\\MenuBundle\\Entity\\Menu',
-                            'attr' => array('class' => 'span5 select2')
-                        ))
-                        ->add('name', 'text')
-                        ->add('lang', 'text', array('required' => false))
-                        ->add('theme', 'choice', array(
-                            'choices' => $this->getThemes(),
-                            'attr' => array('class' => 'span5 select2')
-                        ))
-                        ->add('cache_ttl', 'integer', array(
-                            'required' => false,
-                            'attr' => array('class' => 'span2')
-                        ))
-                        ->add('enabled', 'checkbox', array('required' => false))
+                ->add('menu_id', 'choice', array(
+                    'choices' => $this->getMenusAsArray(),
+                    'attr' => array('class' => 'span5 select2')
+                ))
+                ->add('name', 'text')
+                ->add('lang', 'text', array('required' => false))
+                ->add('theme', 'choice', array(
+                    'choices' => $this->getThemes(),
+                    'attr' => array('class' => 'span5 select2')
+                ))
+                ->add('cache_ttl', 'integer', array(
+                    'required' => false,
+                    'attr' => array('class' => 'span2')
+                ))
+                ->add('enabled', 'checkbox', array('required' => false))
         ;
     }
 
@@ -45,7 +45,7 @@ class BlockMenuConfig extends BlockConfig
 
         $block->setService($this->getService());
         $block->setCategory('Menu');
-        $block->setParameters(json_encode(array('menu_id' => $data['menu']->getId())));
+        $block->setParameters(json_encode(array('menu_id' => $data['menu_id']->getId())));
         $block->setName($data['name']);
         $block->setLang($data['lang']);
         $block->setTheme($data['theme']);
@@ -57,13 +57,11 @@ class BlockMenuConfig extends BlockConfig
 
     public function render(BlockEntity $block, $page = null, $request = null)
     {
-        if ($block->getService() != $this->getService())
-        {
+        if ($block->getService() != $this->getService()) {
             throw new \InvalidArgumentException('Block service not supported');
         }
 
         return $this->httpKernel->forward($block->getTheme(), array('block' => $block, 'page' => $page, 'request' => $request));
-
     }
 
 }
